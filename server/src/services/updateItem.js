3ba7615c2a2bed90bebc78b161939002c84e9229
aryanm5 <aryanm.email@gmail.com>
2023-04-29 16:27:52 -0700

    created updateTabs endpoint. Switched to nodejs14.x for aws-sdk bug

---
+++ server/src/services/updateItem.js
@@ -0,0 +1,34 @@
+import { DynamoDB } from 'aws-sdk';
+
+const docClient = new DynamoDB.DocumentClient();
+
+const table = process.env.TABLE;
+
+const updateItem = async (id, info) => {
+    let expressionAttributeNames = {};
+    let expressionAttributeValues = {};
+    let updateExpression = 'set ';
+    Object.keys(info).forEach(key => {
+        if (info[key] !== undefined) {
+            expressionAttributeNames['#' + key] = key;
+            updateExpression += `#${key} = :${key}, `;
+            expressionAttributeValues[':' + key] = info[key];
+        }
+    });
+    updateExpression = updateExpression.slice(0, -2);
+
+    const params = {
+        TableName: table,
+        Key: {
+            id,
+        },
+        ExpressionAttributeNames: expressionAttributeNames,
+        UpdateExpression: updateExpression,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ReturnValues: 'ALL_NEW',
+    };
+
+    return (await docClient.update(params).promise()).Attributes;
+};
+
+export { updateItem };
