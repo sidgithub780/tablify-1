a611c51565e04619874d2c249b0871de79434cd4
skumar <53668449+sidgithub780@users.noreply.github.com>
2023-04-29 17:37:22 -0700

    only chunks of main text return from the webpages

--- web/background.js
+++ web/background.js
@@ -12,9 +12,30 @@ chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
   console.log("Tab removed:", removeInfo.url);
 });
 
+// https://cdn.jsdelivr.net/npm/readability-js@1.0.7/src/readability.min.js
+
 chrome.tabs.query({}, function (tabs) {
   const functionToExecute = () => {
-    return document.body.innerText;
+    //const text = document.body.innerText;
+
+    const mainTextElements = document.querySelectorAll(
+      "p, h1, h2, h3, h4, h5, h6"
+    );
+    const mainText = Array.from(mainTextElements)
+      .map((element) => element.textContent)
+      .join("\n");
+
+    const chunks = mainText.split(/\n{2,}|\r{2,}|\r\n{2,}/);
+    let largestChunk = "";
+
+    for (let i = 0; i < chunks.length; i++) {
+      const chunk = chunks[i].trim();
+      if (chunk.length > largestChunk.length) {
+        largestChunk = chunk;
+      }
+    }
+
+    return largestChunk;
   };
   for (var i = 0; i < tabs.length; i++) {
     chrome.scripting
