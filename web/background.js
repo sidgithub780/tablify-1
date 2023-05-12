d095b7f80dca796b0367132ca74edcfba102c1b2
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2023-05-11 21:19:03 -0700

    Update background.js

--- web/background.js
+++ web/background.js
@@ -4,7 +4,7 @@ const scrapeData = async (tabURL) => {
   const response = await fetch(url, {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "",
+      "X-RapidAPI-Key": "REDACTED API KEY",
       "X-RapidAPI-Host": "tldr-text-analysis.p.rapidapi.com",
     },
   });
